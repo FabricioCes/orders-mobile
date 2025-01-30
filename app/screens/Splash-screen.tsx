cbebbebddcfd217f0898b06d6c682cd9f8bcf0ac
Alejandro Miranda Rodriguez <lalo.amr.cr@gmail.com>
2025-01-30 11:40:10 -0600

    refactor: remove unused image component from SplashScreen

--- app/screens/Splash-screen.tsx
+++ app/screens/Splash-screen.tsx
@@ -4,10 +4,6 @@ import React from "react";
 export default function SplashScreenAnimated() {
   return (
     <View className="h-screen items-center justify-center bg-white">
-      <Image
-        source={require("@/assets/images/icon.gif")}
-        style={{ width: 200, height: 200 }}
-      />
       <Text>Cargando ...</Text>
     </View>
   );
